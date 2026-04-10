841bd60b1e27dcf8f43234e31da8e7393e1a09da
Trond Jakob Sjøvang <trond@sjovang.no>
2026-04-10 12:26:40 +0200

    demo

--- main.tf
+++ main.tf
@@ -10,15 +10,56 @@ module "connected_organization" {
 }
 
 /* Create access catalog for Entra ID Administration */
+module "catalog" {
+  source           = "./modules/access-catalog"
+  name             = "Demo catalog"
+  description      = "Catalog for Entra ID Administration access packages"
+  external_catalog = true
+  catalog_owner_id = data.azuread_user.tjs.object_id
+}
 
 /* Create access package for Global Administrator role */
+module "global_admin" {
+  source                           = "./modules/access-package"
+  catalog_id                       = module.catalog.catalog_id
+  display_name                     = "Global Administrator Access"
+  description                      = "Global Administrator role in Entra ID"
+  backup_approvers_group_object_id = module.catalog.backup_approvers_group_object_id
+}
 
-/* Modify policy rules for the Global Administrator role */
+module "global_admin_role_policy" {
+  source                       = "./modules/entraid-pim-role-policy"
+  role_name                    = "Global Administrator"
+  user_activation_max_duration = "PT1H"
+}
 
 /* Create group for users with access to global administrator role */
-
+resource "msgraph_resource" "global_admins" {
+    url = "groups"
+    body = {
+        displayName       = "PIM Global Admin role"
+        description        = "Grant access to Global Administrator role via PIM"
+        mailEnabled = false
+        mailNickname = "pimglobaladmins"
+        securityEnabled   = true
+        isAssignableToRole = true
+    }
+    response_export_values = {
+      all = "@"
+    }
+}
 /* assign group access to global administrator role via PIM */
+resource "azuread_directory_role_eligibility_schedule_request" "global_admin" {
+  role_definition_id = module.global_admin_role_policy.id
+  principal_id       = msgraph_resource.global_admins.output.all.id
+  directory_scope_id = "/"
+  justification      = "demo"
+}
 
 /* Associate group with access catalog and access package */
-
-/* Create access catalog for Entra ID Administration */
+module "global_admin_assoc" {
+  source            = "./modules/group-access-package-association"
+  catalog_id        = module.catalog.catalog_id
+  access_package_id = module.global_admin.access_package_id
+  group_object_id   = msgraph_resource.global_admins.output.all.id
+}
